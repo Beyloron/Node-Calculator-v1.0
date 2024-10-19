fe078584aac78112616a1b11aeae49c2098f87b3
John Robertson <johnmrobertson28@gmail.com>
2024-10-19 11:40:00 -0500

    Fix empty input bug on index.js. Refactor index.js to follow DRY principles

--- bonus.js
+++ bonus.js
@@ -1,28 +1,26 @@
-var rs = require('readline-sync');
-
-
+var rs = require("readline-sync");
 
 const operators = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
 };
 
 const cleanUpString = (str) => {
-  return str.replaceAll(' ', '');
-}
+  return str.replaceAll(" ", "");
+};
 
 const validateExpression = (expr, arrOperators) => {
   const cleanedExpr = cleanUpString(expr);
   const isThereFirstNumber = !arrOperators.some(
-    (item) => cleanedExpr.indexOf(item) === 0,
+    (item) => cleanedExpr.indexOf(item) === 0
   );
   const isThereSecondNumber = !arrOperators.some(
-    (item) => cleanedExpr.indexOf(item) === expr.length - 1,
+    (item) => cleanedExpr.indexOf(item) === expr.length - 1
   );
-  const isOperatorValid = arrOperators.some(
-    (item) => cleanedExpr.includes(item),
+  const isOperatorValid = arrOperators.some((item) =>
+    cleanedExpr.includes(item)
   );
 
   return isThereFirstNumber && isThereSecondNumber && isOperatorValid;
@@ -33,12 +31,12 @@ const parseExpression = (arrOperators) => {
   const cleanedExpr = cleanUpString(userInput);
   if (validateExpression(userInput, arrOperators)) {
     const operatorIndex = cleanedExpr.indexOf(
-      arrOperators.find((item) => cleanedExpr.includes(item)),
+      arrOperators.find((item) => cleanedExpr.includes(item))
     );
     const firstNumber = +cleanedExpr.substring(0, operatorIndex);
     const secondNumber = +cleanedExpr.substring(
       operatorIndex + 1,
-      cleanedExpr.length,
+      cleanedExpr.length
     );
     return [firstNumber, cleanedExpr[operatorIndex], secondNumber];
   } else {
@@ -54,33 +52,7 @@ const calculator = (initData) => {
   const validOperators = Object.keys(initData);
   const [firstNum, operator, secondNum] = parseExpression(validOperators);
   const result = calculate(initData, firstNum, operator, secondNum);
-  console.log('The result is:', result);
+  console.log("The result is:", result);
 };
 
 calculator(operators);
-
-
-
-// let num1 = '';
-// let operator = '';
-// let num2 = '';
-
-// for (let char of userInput) {
-//   if (!isNaN(char)) {
-//     if (!operator) {
-//       num1 += char;
-//     } else {
-//       num2 += char;
-//     }
-//   } else if (char in operators) {
-//     operator = char;
-//   }
-// };
-
-// if (operators[operator]) {
-//   const result = operators[operator](parseInt(num1), parseInt(num2));
-//   console.log(`Result: ${result}`)
-// } else {
-//   console.log(`Invalid input. Please enter a valid mathematical expression`)
-// };
-
--- index.js
+++ index.js
@@ -1,46 +1,23 @@
-var rs = require('readline-sync');
+const rs = require("readline-sync");
 
 const operators = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
 };
 
-const operator = () => {
-  let userOp;
-  while (true) {
-    userOp = rs.question(`What operation would you like to perform? `);
-    if (userOp in operators) {
-      return userOp;
-    } else {
-      console.log(`That is not a valid operator.`);
-    }
-  }
+const getOperator = (limit) => {
+  return rs.question(`What operation would you like to perform? `, {
+    limit,
+    limitMessage: `That is not a valid operator.`,
+  });
 };
 
-const num1 = () => {
-  let userInput;
-  while (true) { // This one took a minute, before it was returning the input even if it wasn't a valid input. Same as on operator()
-    userInput = rs.question(`Enter the first number: `);
-    if (!isNaN(userInput)) { // Nested the return statement to only invoke if input is a number
-      return Number(userInput);
-    } else {
-      console.log(`This is not a number.`);
-    }
-  }
-};
-
-const num2 = () => {
-  let userInput;
-  while (true) {
-    userInput = rs.question(`Enter the second number: `);
-    if (!isNaN(userInput)) {
-      return Number(userInput);
-    } else {
-      console.log(`This is not a number.`);
-    }
-  }
+const getNumber = (order) => {
+  return rs.questionInt(`Enter the ${order} number: `, {
+    limitMessage: "That is not a valid number!",
+  });
 };
 
 const evaluate = (initData, num1, op, num2) => {
@@ -48,50 +25,12 @@ const evaluate = (initData, num1, op, num2) => {
 };
 
 const calculator = (initData) => {
-  const op = operator();
-  const firstNumber = num1();
-  const secondNumber = num2();
-  const result = evaluate(initData, firstNumber, op, secondNumber);
+  const operatorsList = Object.keys(initData);
+  const operator = getOperator(operatorsList);
+  const firstNumber = getNumber("first");
+  const secondNumber = getNumber("second");
+  const result = evaluate(initData, firstNumber, operator, secondNumber);
   console.log(`The result is: ${result}`);
 };
 
 calculator(operators);
-
-
-
-
-
-
-
-
-
-
-
-// const userInput = () => {
-//   const userOp = rs.question(`What operation would you like to perform? `)
-//   if (userOp in operators) {
-//     const num1Input = rs.question(`Enter the first number: `);
-//     const num1 = parseFloat(num1Input);
-//     if (!isNaN(num1)) {
-//       const num2Input = rs.question(`Enter the second number: `);
-//       const num2 = parseFloat(num2Input);
-//       if (!isNaN(num2)) {
-//         const result = operators[userOp](num1, num2);
-//         console.log(`The result is: ${result}`);
-//         return result;
-//       } else {
-//         console.log('Invalid input. Please enter a number for the second operand.');
-//         userInput();
-//       }
-//     } else {
-//       console.log('Invalid input. Please enter a number for the first operand.');
-//       userInput();
-//     }
-//   } else {
-//     console.log(`That is not a valid operator.`);
-//     userInput();
-//   }
-// };
-
-// userInput();
-
